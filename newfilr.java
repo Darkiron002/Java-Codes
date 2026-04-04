c215c103cc159fb3af1ad4f2e1342403f42d107c
Darkiron002 <chayanmistry14@gmail.com>
2026-04-04 14:38:17 +0530

    The retangle pattern

--- newfilr.java
+++ newfilr.java
@@ -1,5 +1,22 @@
-class newfilr {
+
+import java.util.*;
+
+public class newfilr {
     public static void main(String args[]){
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for(int i = 1;i<=n;i++){
+            for(int j= 1; j<=m; j++){
+                if(i==1||j== 1||i==n||j== m){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+        sc.close();
     }
 }
