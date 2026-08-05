9c5cde16206a222855a94bac98dffdc114ecadd1
Darkiron002 <chayanmistry14@gmail.com>
2026-08-05 19:55:07 +0530

    Random object and methods

--- newfilr.java
+++ newfilr.java
@@ -1,19 +1,45 @@
-
-import java.util.*;
+import java.util.Scanner;
+import java.util.Random;
 
 public class newfilr {
-    public static int avg(int a,int b, int c){
-        return (a+b+c)/3;
-    }
-    public static void main(String args[]){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("The first number - ");
-        int a = sc.nextInt();
-        System.out.print("The Sceond number - ");
-        int b = sc.nextInt();
-        System.out.print("The Third number - ");
-        int c = sc.nextInt();
-        System.out.print("the average of the three numbers is - "+ avg(a,b,c));
+        Random random = new Random();
+
+        int num = random.nextInt(1,7);
+        switch (num) {
+            case 1:
+                System.out.println("you got a 1 in dice");
+                break;
+            case 2:
+                System.out.println("you got a 2 in dice");
+                break;
+            case 3:
+                System.out.println("you got a 3 in dice");
+                break;
+        
+            default:
+                System.out.println("nothing");
+                break;
+        }
+
+        boolean isHead = random.nextBoolean();
+        if(isHead){
+            System.out.println("you got heads ");
+        }else{
+            System.out.println("you got tails");
+        }
+
+
+
+
+
+
+
+
+        System.out.print("Enter your number : ");
+        int num2 = sc.nextInt();
+        System.out.print("The squre root of the number is - "+StrictMath.sqrt(num2));
         sc.close();
     }
 }
