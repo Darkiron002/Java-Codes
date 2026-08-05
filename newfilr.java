f64de55d43dd14dc1c42a46bd2236639af045d23
Darkiron002 <chayanmistry14@gmail.com>
2026-08-05 22:09:42 +0530

    Everything about Math class

--- newfilr.java
+++ newfilr.java
@@ -1,45 +1,32 @@
 import java.util.Scanner;
-import java.util.Random;
+
 
 public class newfilr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random random = new Random();
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
 
-        int num = random.nextInt(1,7);
-        switch (num) {
-            case 1:
-                System.out.println("you got a 1 in dice");
-                break;
-            case 2:
-                System.out.println("you got a 2 in dice");
-                break;
-            case 3:
-                System.out.println("you got a 3 in dice");
-                break;
-        
-            default:
-                System.out.println("nothing");
-                break;
-        }
+        double squre;
+        squre = Math.pow(2,4);
 
-        boolean isHead = random.nextBoolean();
-        if(isHead){
-            System.out.println("you got heads ");
-        }else{
-            System.out.println("you got tails");
-        }
+        System.out.println("The round of 3.14 is: "+Math.round(3.14)); // rounds upto the cosest integer 
+        System.out.println("The absolute value of *5 is : "+Math.abs(-5));// gives the absolute value of the number given below
+        System.out.println("The squre root fo 9 is: "+Math.sqrt(9)); // gives the squre root of the number
+        System.out.println("The bigger round up of 3.14 is: "+Math.ceil(3.14)); // rounds up to the biggest number 
+        System.out.println("The bigger number is: "+Math.max(10,20)); //gives the  maximum number between two
+        System.out.println("The smaller number is: "+Math.min(10, 10)); // gives the minimum number between two
+        System.out.println(squre);
 
+        // creating a program to find the area of a circle
 
+        double r ;
+        System.out.print("Enter the radius of the circle : ");
+        r= sc.nextDouble();
+        System.out.println("The area is : "+Math.PI*Math.pow(r,2)+"cm²");
 
+        // creating a program to find 
 
-
-
-
-
-        System.out.print("Enter your number : ");
-        int num2 = sc.nextInt();
-        System.out.print("The squre root of the number is - "+StrictMath.sqrt(num2));
         sc.close();
     }
 }
