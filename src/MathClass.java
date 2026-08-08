b3a1ee5821bda2504a9aaa91989b40eb01c5d0f1
Darkiron002 <chayanmistry14@gmail.com>
2026-08-08 14:38:55 +0530

    Printing retangle

---
+++ src/MathClass.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class MathClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double squre;
+        squre = Math.pow(2,4);
+
+        System.out.println("The round of 3.14 is: "+Math.round(3.14)); // rounds upto the cosest integer 
+        System.out.println("The absolute value of *5 is : "+Math.abs(-5));// gives the absolute value of the number given below
+        System.out.println("The squre root fo 9 is: "+Math.sqrt(9)); // gives the squre root of the number
+        System.out.println("The bigger round up of 3.14 is: "+Math.ceil(3.14)); // rounds up to the biggest number 
+        System.out.println("The bigger number is: "+Math.max(10,20)); //gives the  maximum number between two
+        System.out.println("The smaller number is: "+Math.min(10, 10)); // gives the minimum number between two
+        System.out.println(squre);
+
+        // creating a program to find the area of a circle
+
+        double r ;
+        System.out.print("Enter the radius of the circle : ");
+        r= sc.nextDouble();
+        System.out.printf("The area is : %.0f cm²", Math.PI*Math.pow(r,2));
+
+        // creating a program to find 
+
+        sc.close();
+    }
+}
