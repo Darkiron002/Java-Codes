229ec7bf99e2f2dfcc088e88cb6b20c2a686ad4b
Darkiron002 <chayanmistry14@gmail.com>
2026-08-14 23:43:48 +0530

    Array basics and methods

--- src/Array.java
+++ src/Array.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.Arrays;
 
-public class newfilr {
+public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
