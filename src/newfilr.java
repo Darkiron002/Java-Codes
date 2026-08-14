0db58178bba1a19f4de5c8030de64b745b415e67
Darkiron002 <chayanmistry14@gmail.com>
2026-08-15 01:13:24 +0530

    Variable argument method for method overload

--- src/newfilr.java
+++ src/newfilr.java
@@ -1,32 +1,22 @@
 import java.util.Scanner;
 import java.util.Random;
-import java.util.Arrays;
 
 public class newfilr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
-        int[] numbers = {4,6,3,2,5,13,2,9};
-        int target = 55;
-        boolean isFound = false;
-
-        // seraching the target in an array
-        for(int i =0;i<numbers.length; i++){
-            if(numbers[i] == target){
-                System.out.println("You found the number at "+i);
-                isFound=true;
-                break;
-            }else{
-                System.out.println("its not target ");
-
-            }
-        }
-        if(!isFound){
-            System.out.println("the entire array doesn't contain the number ");
-        }
-
-
+        
+        //function over loead solution 
+        System.out.println(add(1,2,3,6,4));
         sc.close();
+    }
+
+    static int add(int... number){
+        int sum =0; 
+        for(int num : number){
+            sum +=num;
+        }
+        return sum;
     }
 }
