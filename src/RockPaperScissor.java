a71ca0928440a3f2349d1ff7643c645eed6077d5
Darkiron002 <chayanmistry14@gmail.com>
2026-08-16 16:47:45 +0530

    Finding root of a quadratinc equation

---
+++ src/RockPaperScissor.java
@@ -0,0 +1,36 @@
+/**
+ * RockPaperScissor
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissor {
+
+    public static void main(String[] args) {
+        //scanner and random declearation 
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        //otions array 
+        String[] choices = {"Rock ","Paper","Scissor"};
+
+
+        //Variable declearation 
+        String pChoice ;
+        String cChoice;
+
+
+
+
+        System.out.println("******************************************");
+        System.out.println("This is the Rock Paper Scissor game!!!! ");
+        System.out.println("******************************************");
+        System.out.println();
+
+
+
+
+
+       sc.close(); 
+    }
+}
--- src/quadRoot.java
+++ src/quadRoot.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class newfilr {
+public class quadRoot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
