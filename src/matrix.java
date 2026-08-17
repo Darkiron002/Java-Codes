1285a3f1b5148694abb519de3e7f7516102b01e2
Darkiron002 <chayanmistry14@gmail.com>
2026-08-18 00:15:06 +0530

    Matrix addition

--- src/matrix.java
+++ src/matrix.java
@@ -11,36 +11,45 @@ public class matrix {
         System.out.print("Enter the number of columns: ");
         int col = sc.nextInt();
 
-        // int[][] mat1 = {{1,2,3},{4,5,6},{7,8,9}};
         int [][] mat1 = new int[row][col];
         System.out.println("Now enter the elements !!!!");
         //For user input
         for( int i = 0;i<row;i++){
             for(int j = 0;j<col;j++){
-                System.out.print("Enter the element on "+i+" and "+j);
+                System.out.print("Enter the element on "+i+" and "+j+" : ");
                 mat1[i][j] = sc.nextInt();
             }System.out.println();
         }
 
-        for( int i = 0;i<row;i++){
+        /*for( int i = 0;i<row;i++){
             for(int j = 0;j<col;j++){
                 System.out.print(mat1[i][j] +" ");
             }System.out.println();
-        } 
+        } */
         System.out.println();
-        // int[][] mat2 = {{0,9,8},{7,6,5},{4,3,2}};
-        // int[][] mat3 = new int[3][3];
+        System.out.println("Now Enter the second Matrix");
 
-        // for(int i=0;i<3;i++){
-        //     for(int j=0;j<3;j++){
-        //         mat3[i][j]= mat1[i][j]+mat2[i][j];
-        //     }
-        // }
-        // for( int i = 0;i<3;i++){
-        //     for(int j = 0;j<3;j++){
-        //         System.out.print(mat3[i][j] +" ");
-        //     }System.out.println();
-        // }
+        int[][] mat2 = new int[row][col];
+        // Getting the elements 
+        for( int i = 0;i<row;i++){
+            for(int j = 0;j<col;j++){
+                System.out.print("Enter the element on "+i+" and "+j+" : ");
+                mat2[i][j] = sc.nextInt();
+            }System.out.println();
+        }   
+        int[][] mat3 = new int[row][col];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                mat3[i][j]= mat1[i][j]+mat2[i][j];
+            }
+        }
+        System.out.println("the New matrix with the sum of the two is- ");
+        for( int i = 0;i<3;i++){
+            for(int j = 0;j<3;j++){
+                System.out.print(mat3[i][j] +" ");
+            }System.out.println();
+        }
 
         sc.close();
 
